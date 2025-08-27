66ccbfe478fcea5eb0d262f2994e750fd2ba7257
salissadiq <salissadiq@gmail.com>
2025-08-27 16:01:00 +0100

    Add columns for title, content, and user_id to posts table migration

--- database/migrations/2025_08_26_154050_create_posts_table.php
+++ database/migrations/2025_08_26_154050_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
